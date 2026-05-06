62cdfa7fb8dd4dc020468ee5538f5a8473d9d9b4
WenHan <willy870719@gmail.com>
2026-05-06 13:21:01 +0800

    feat: 新增跳轉回 EWova 帶有資訊

--- Assets/EWova.Core/Runtime/EWova.cs
+++ Assets/EWova.Core/Runtime/EWova.cs
@@ -1,22 +1,56 @@
+using EWova.NetService;
+
 using UnityEngine;
 
 namespace EWova
 {
+    [System.Flags] 
+    public enum DeepLinkQueryInclude
+    {
+        None = 0,
+        LoginToken = 1 << 0,
+        WorldID = 1 << 1,
+        SpaceID = 1 << 2,
+
+        Default = LoginToken | WorldID | SpaceID
+    }
+
     public static class EWova
     {
         public const string NetServiceApi = "https://dash.ewova.com/api/";
         public const string QueryPrefix = "ewova";
-        
-        private const string DeepLink = "ewova://";
 
-        public static void LaunchApp()
+        private const string DeepLinkScheme = "ewova://";
+
+        public static void LaunchApp() 
+            => LaunchApp(DeepLinkQueryInclude.Default);
+        public static void LaunchApp(DeepLinkQueryInclude include)
         {
-            Application.OpenURL(GetDeepLink());
+            Application.OpenURL(GetDeepLink(include));
         }
 
-        public static string GetDeepLink() 
+        public static string GetDeepLink()
+            => GetDeepLink(DeepLinkQueryInclude.Default);
+        public static string GetDeepLink(DeepLinkQueryInclude include)
         {
-            return DeepLink;
+            var path = EWovaUriPath.Parse(DeepLinkScheme);
+
+            if ((include & DeepLinkQueryInclude.LoginToken) != 0)
+            {
+                path.AddOrSetQueue(EWovaUriPath.Query.Token);
+            }
+
+            if ((include & DeepLinkQueryInclude.WorldID) != 0)
+            {
+                path.AddOrSetQueue(EWovaUriPath.Query.WorldID);
+            }
+
+            if ((include & DeepLinkQueryInclude.SpaceID) != 0)
+            {
+                path.AddOrSetQueue(EWovaUriPath.Query.SpaceID);
+            }
+
+            return path.GetResult().ToString();
         }
     }
 }
