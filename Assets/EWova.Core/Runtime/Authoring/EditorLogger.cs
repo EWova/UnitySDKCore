451671c054e47dccb951460880053c12bc56a7f5
WenHan <willy870719@gmail.com>
2026-06-12 13:15:25 +0800

    feat: EditorLogger HideCallstack

--- Assets/EWova.Core/Runtime/Authoring/EditorLogger.cs
+++ Assets/EWova.Core/Runtime/Authoring/EditorLogger.cs
@@ -1,19 +1,28 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
 namespace EWova.Authoring
 {
     public static class EditorLogger
     {
         private readonly static Logger _logger = new Logger("Editor [EWova] ", LogLevel.Full);
         public static ILogSource Source => _logger;
+        public readonly static HashSet<string> Flags;
+
+        [HideInCallstack]
         public static void Info(string message)
         {
             if (_logger.InfoEnabled)
                 _logger.Info(message);
         }
+        [HideInCallstack]
         public static void Warning(string message)
         {
             if (_logger.WarnEnabled)
                 _logger.Warn(message);
         }
+        [HideInCallstack]
         public static void Error(string message)
         {
             if (_logger.ErrorEnabled)
