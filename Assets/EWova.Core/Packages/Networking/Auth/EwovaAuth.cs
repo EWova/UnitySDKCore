cc42b06ba49947195bf5a2b12ec7185bc55f1fb8
WenHan <willy870719@gmail.com>
2026-07-07 11:34:20 +0800

    update

--- Assets/EWova.Core/Packages/Networking/Auth/EwovaAuth.cs
+++ Assets/EWova.Core/Packages/Networking/Auth/EwovaAuth.cs
@@ -2,7 +2,7 @@ namespace EWova.Auth
 {
     public class EWovaAuth : AuthProvider
     {
-        private EWovaAuth(EWovaAuthConfig authConfig) : base(authConfig)
+        internal EWovaAuth(EWovaAuthConfig authConfig) : base(authConfig)
         {
         }
 
@@ -13,6 +13,6 @@ namespace EWova.Auth
                 throw new System.Exception("目前不支援 EWovaAuth");
             }
         }
-        private static EWovaAuth _instance;
+        private readonly static EWovaAuth _instance;
     }
 }
