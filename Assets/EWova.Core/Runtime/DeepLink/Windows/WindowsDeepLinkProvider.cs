e25514f080b1f23970b2d2d7bc78fd5e8e17c9c7
WenHan <willy870719@gmail.com>
2026-08-04 17:36:53 +0800

    修改報錯位置

--- Assets/EWova.Core/Runtime/DeepLink/Windows/WindowsDeepLinkProvider.cs
+++ Assets/EWova.Core/Runtime/DeepLink/Windows/WindowsDeepLinkProvider.cs
@@ -38,13 +38,13 @@ namespace EWova.DeepLink.Win
         public bool ConfigureScheme(string scheme, out string errorMessage)
         {
             errorMessage = $"Windows DeepLink 在此平台不受支援，無法配置 Scheme: {scheme}";
-            return false;
 #if UNITY_EDITOR_WIN && (NET_STANDARD_2_0 || NET_STANDARD_2_1)
             if (Authoring.DevelopTip.IsEnabled)
             {
                 Authoring.EditorLogger.Warn("Windows DeepLink 在編輯器的 .NET Standard 2.x 模式下不受支援，請到 ProjectSettings/Player/OtherSettings/Configuration/ApiCompatibilityLevel 切換到 .NET Framework 以啟用此功能");
             }
 #endif
+            return false;
         }
 #endif
     }
